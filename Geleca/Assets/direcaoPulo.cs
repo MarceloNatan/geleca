18a9947c06495442719093a7d1ad6e444fb992d0
MarceloNatan <marcelonvasc@gmail.com>
2018-09-14 19:49:12 -0300

    Linha de Debug
    Linha de Debug  do jogador até o ponteiro

--- Geleca/Assets/direcaoPulo.cs
+++ Geleca/Assets/direcaoPulo.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class direcaoPulo : MonoBehaviour {
     public Vector3 posicaoMouse;
     public float velocidadeMovi = .1f;
+    public Vector2 minhaPosicao;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+         minhaPosicao = new Vector2(transform.position.x, transform.position.y);
+
+        //-------------------- Rotação da seta de direção do salto
         posicaoMouse = Input.mousePosition;
         posicaoMouse = Camera.main.ScreenToWorldPoint(posicaoMouse);
         Vector2 direcao = new Vector2(posicaoMouse.x - transform.position.x , 
                                       posicaoMouse.y - transform.position.y);
 
         transform.up = direcao;
+
+
+        //-------------------- Linha de Debug do jogador até o ponteiro do mouse
+        Color cor = new Color(201, 0, 0);
+        Debug.DrawLine(minhaPosicao, posicaoMouse, cor);
     }
 }
