1a87d095b512bc4023d13eb9f22b1339fc9b2f6d
MarceloNatan <marcelonvasc@gmail.com>
2018-09-14 19:15:08 -0300

    Adicionando projeto ao Git
    Projeto adicionado ao git, neste commit temos uma versão simplificada de movimentos da geleca e uma seta direcional que será "linkada" ao analógico direito do controle, a priori, a seta aponta para o cursor.

---
+++ Geleca/Assets/Scripts/ControlePersonagem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlePersonagem : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float velocidade = 7;
+    //public float velPulo = 7;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float movimentoHorizontal = Input.GetAxis("Horizontal");
+        float movimentoVertical = Input.GetAxis("Vertical");
+
+        Vector2 movimentacao = new Vector2(movimentoHorizontal, movimentoVertical);
+
+        rb.AddForce(movimentacao * velocidade);
+	}
+}
---
+++ Geleca/Assets/direcaoPulo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class direcaoPulo : MonoBehaviour {
+    public Vector3 posicaoMouse;
+    public float velocidadeMovi = .1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        posicaoMouse = Input.mousePosition;
+        posicaoMouse = Camera.main.ScreenToWorldPoint(posicaoMouse);
+        Vector2 direcao = new Vector2(posicaoMouse.x - transform.position.x , 
+                                      posicaoMouse.y - transform.position.y);
+
+        transform.up = direcao;
+    }
+}
