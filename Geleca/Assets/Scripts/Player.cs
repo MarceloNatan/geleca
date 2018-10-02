4347f0f8e2912fa6b5f539c04acd5f9c0f5a1bcb
Marcelo Natan <marcelonvasc@gmail.com>
2018-10-02 00:30:59 -0300

    Personagem adere a superfície
    Personagem adere a superfície.
    
    NOTA: a versão do Unity atualizou, sugiro fazer a atualização aí.!

--- Geleca/Assets/Scripts/Player.cs
+++ Geleca/Assets/Scripts/Player.cs
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour {
     [SerializeField]
     private GameObject tiro;
 
+    private bool noMuro = false;
+
     private byte countTiro = 0;
 
 	// Use this for initialization
@@ -77,11 +79,66 @@ public class Player : MonoBehaviour {
             
         }
 
+        //-----------------------------------HOOOOOO------------------------------
+        if (noMuro)
+        {
+            if (Input.GetAxis("Vertical") > 0.01f)
+            {
+                //   Debug.Log("virou miserá");
+                //  transform.Rotate(Vector3.right* Time.deltaTime);
+                //transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+              
+                transform.position += new Vector3(0, 0.05f, 0);
+            
+
+                direcao = -1;
+                if (jumping)
+                    GetComponent<Rigidbody2D>().AddForce(new Vector3(-3, 0, 0));
+                //else
+                //transform.position -= new Vector3(0.05f, 0, 0);
+
+
+            }
+            if (Input.GetAxis("Vertical") < -0.01f)
+            {
+                Debug.Log("virou miserá");
+                //  transform.Rotate(Vector3.right* Time.deltaTime);
+                //transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+                transform.eulerAngles = new Vector3(0, 0, -90);
+                transform.position -= new Vector3(0, 0.05f, 0);
+                direcao = -1;
+
+                //if (jumping)
+                //    GetComponent<Rigidbody2D>().AddForce(new Vector3(0, 0, 0));
+                //else
+                //transform.position -= new Vector3(0.05f, 0, 0);
+
+
+            }
+        }
+        else if(noMuro == false){
+            this.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+              }
+
+        //-----------------------------------------------------------------
+
         if(Input.GetAxis("Fire1") != 0 && !jumping)
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector3(0,500,0));
             jumping = true;
+           
         }
+
+        if (Input.GetAxis("Fire1") != 0 && noMuro)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector3(500, 0, 0));
+            noMuro = false;
+            Debug.Log(noMuro);
+         
+
+        }
+
+
 
         if(Input.GetAxis("Fire2")>0 && countTiro == 0)
         {
@@ -100,7 +157,7 @@ public class Player : MonoBehaviour {
             countTiro = (byte)((countTiro+1) % 10);    
         }
 
-
+        //Debug.Log(jumping);
         
 
         //Debug.Log(Input.GetAxis("Jump"));
@@ -112,10 +169,30 @@ public class Player : MonoBehaviour {
         if (collision.gameObject.tag == "Terrain")
         {
             jumping = false;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+            this.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
         }
 
+        if (collision.gameObject.tag == "Paredes")
+        {
+            transform.eulerAngles = new Vector3(0, 0, -90);
+            noMuro = true;
+            this.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+        }
+
+        if (collision.gameObject.tag == "ParedeDireita")
+        {
+            transform.eulerAngles = new Vector3(0, 0, 90);
+            noMuro = true;
+            this.GetComponent<Rigidbody2D>().gravityScale = 0.0f;
+        }
+
+
         Debug.Log(collision.gameObject.tag);
+        
     }
+
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
